568a94853f8323d95ee2fc13d62df58048ea30ae
amisavani39 <amisavani43@gmail.com>
2026-01-28 20:59:34 +0530

    background image add

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach((faq) => {
+faqs.forEach(faq => {
   faq.querySelector(".question").addEventListener("click", () => {
-    faqs.forEach((item) => {
-      if (item !== faq) {
+
+    faqs.forEach(item => {
+      if(item !== faq){
         item.classList.remove("active");
-        item.querySelector(".icon").textContent = "+";
+        item.querySelector(".toggle").src = "images/icon-plus.svg";
       }
     });
 
     faq.classList.toggle("active");
 
-    const icon = faq.querySelector(".icon");
-    icon.textContent = faq.classList.contains("active") ? "−" : "+";
+    const icon = faq.querySelector(".toggle");
+    icon.src = faq.classList.contains("active")
+      ? "images/icon-minus.svg"
+      : "images/icon-plus.svg";
   });
 });
